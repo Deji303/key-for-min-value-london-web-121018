8e279b12bc2736db57dac96105f5d83805d085c2
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-11-25 13:20:40 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,15 @@
 def key_for_min_value(name_hash)
   lowest = 10000; 
   lowest_key = ""; 
-  name_hash.each do |key,value|
-    if value < lowest
-      lowest = value; 
-      lowest_key = key; 
-    end 
+  if name_hash == {}
+    nil 
   end 
+  else 
+    name_hash.each do |key,value|
+      if value < lowest
+        lowest = value; 
+        lowest_key = key; 
+      end 
+    end 
   lowest_key
 end
